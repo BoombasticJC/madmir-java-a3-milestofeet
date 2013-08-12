c0edf4f4b76b02e25fdfd7325634010fb9b6a00d
Carney <jc.augustine@live.ca>
2013-08-12 13:53:46 -0400

    Heres my assignment; didnt add the JOption thou.

--- src/MilesToFeetInteractive.java
+++ src/MilesToFeetInteractive.java
@@ -1,12 +1,27 @@
+import java.util.Scanner;
 
 public class MilesToFeetInteractive {
 
 	public static void main(String[] args) {
-		// -------INSTRUCTIONS-------
-		// 1. Refer to exercise 4 on page 113 of the Joyce Farrell book (chapter 2 PDF)
-		// 2. In this file, complete part (b) of this exercise
-		// 3. For this class, you WILL receive an input from the user.
-
+		//Declare variable and initialize name constant FEET_PER_MILE
+		final int FEET_PER_MILE = 5280;
+		float uncleFeet, uncleMiles;
+		
+		//Comment :) 
+		System.out.println("Lets Start To Convert");
+		
+		//Initialized Scanner
+		Scanner kbrd = new Scanner(System.in);
+		
+		//User Input
+		System.out.print("Enter Number In Miles: ");
+		uncleMiles = kbrd.nextFloat();
+		
+		//Convert Miles to Feet
+		uncleFeet = uncleMiles * FEET_PER_MILE;
+		
+		//System Output
+		System.out.println("The distance between your house and your distanation is, " + uncleFeet + "Ft!");
 	}
 
 }
